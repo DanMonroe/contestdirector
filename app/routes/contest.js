1b1c7bac29ca4a6868ba943fe9d0dce53f24a6f1
Dan Monroe <daniel.monroe@cardinalhealth.com>
2015-07-20 19:40:43 -0400

    small update

--- app/routes/contest.js
+++ app/routes/contest.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
 
-    var test = this.store.find('contest', { urlpath: params.urlpath });
-    return test;
+    var selectedContest = this.store.find('contest', { urlpath: params.urlpath });
+    //debugger;
+    return selectedContest;
   }
 });
