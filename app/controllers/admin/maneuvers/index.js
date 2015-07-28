77f8436c738e398cd4944acf97bc09e3114174ad
Dan Monroe <daniel.monroe@cardinalhealth.com>
2015-07-27 22:56:03 -0400

    Maneuvers index

---
+++ app/controllers/admin/maneuvers/index.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+import ManeuversBaseController from './base';
+
+export default ManeuversBaseController.extend({
+    //filteredManeuvers: Ember.computed('selectedItem', function() {
+    //    var selectedClass = this.get('selectedItem');
+    //
+    //    return this.get('model.maneuvers').filter(
+    //        function (maneuver) {
+    //            return maneuver.get('pilotClass.id') === selectedClass;
+    //        }
+    //    );
+    //})
+
+    //actions: {
+    //    deleteManeuver(maneuver) {
+    //        maneuver.destroyRecord().then(() => {
+    //            this.transitionToRoute('admin.maneuvers.index');
+    //        });
+    //    },
+    //    editManeuver(maneuver) {
+    //        this.transitionToRoute('admin.maneuvers.edit', maneuver);
+    //        return false;
+    //    }
+    //
+    //}
+
+
+});
